40c7b5ad99891a8803d01e1f10159c6ae627b64d
Sakshi Chimurkar <sakshichimurkar2@gmail.com>
2026-09-19 17:04:40 +0530

    Update ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewReport from './pages/InterviewReport'
 import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 
-export const ServerUrl = 'http://localhost:8000'
+export const ServerUrl = 'https://interviewiq-xxgb.onrender.com'
 
 function App() {
   const dispatch = useDispatch()
